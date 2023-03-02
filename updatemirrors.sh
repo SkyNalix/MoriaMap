4b3cb0871b143c71cfcf1cb4b51ab2a57daccfd7
Adrian H <adrian.heouairi@etu.u-paris.fr>
2023-03-02 22:17:15 +0100

    Improve mirror script

--- updatemirrors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf moriamap.git
-git clone --mirror https://gaufre.informatique.univ-paris-diderot.fr/tazouev/moriamap.git
-cd moriamap.git
-git remote set-url --push origin https://github.com/SkyNalix/MoriaMap.git
-git fetch -p origin
-git push --mirror
-cd ..
-rm -rf moriamap.git
