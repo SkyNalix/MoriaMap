8b9c74c3264c81e086e12ec37d209ec07d142e71
DERVISHI <sevidervishi@outlook.fr>
2023-03-17 13:48:52 +0100

    Implemented equals and hashCode in Geographicvertex class.

--- src/test/java/dev/moriamap/model/GeographicVertexTest.java
+++ src/test/java/dev/moriamap/model/GeographicVertexTest.java
@@ -100,4 +100,38 @@ public class GeographicVertexTest {
           () -> GeographicVertex.at(null)
         );
     }
+
+    @Test void equalsOnItselfReturnsTrue() {
+        GeographicVertex v = GeographicVertex.at(58.134, 22.4);
+        assertEquals(v,v);
+    }
+
+    @Test void semanticallyEqualInstancesAreEqual() {
+        GeographicVertex u = GeographicVertex.at(58.134, 22.4);
+        GeographicVertex v = GeographicVertex.at(58.134, 22.4);
+        assertEquals(u,v);
+    }
+
+    @Test void semanticallyUnequalInstancesAreNotEqual() {
+        GeographicVertex u = GeographicVertex.at(58.134, 22.4);
+        GeographicVertex v = GeographicVertex.at(58.134, 12.4);
+        assertNotEquals(u,v);
+    }
+
+    @Test void GeographicVertexIsNotEqualToNull() {
+        GeographicVertex v = GeographicVertex.at(58.134, 12.4);
+        assertNotEquals(v,null);
+    }
+
+    @Test void instanceOfDifferentClassIsNotEqual() {
+        GeographicVertex v = GeographicVertex.at(75.0,90.0);
+        Object o = new Object();
+        assertNotEquals(v,o);
+    }
+
+    @Test void hashCodeOfEqualObjectsIsEqual() {
+        GeographicVertex u = GeographicVertex.at(88.134, 25.4);
+        GeographicVertex v = GeographicVertex.at(88.134, 25.4);
+        assertEquals(u.hashCode(), v.hashCode());
+    }
 }
