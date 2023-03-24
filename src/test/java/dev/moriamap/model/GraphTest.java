eb2b522f2856fb7bd86ee0d3ab117c4930ddcddf
MORON USON guillermo <guillermo.moron@laposte.net>
2023-03-24 11:45:46 +0100

    Resolve "Add a map of Vertex to outgoing Edges in Graph"

--- src/test/java/dev/moriamap/model/GraphTest.java
+++ src/test/java/dev/moriamap/model/GraphTest.java
@@ -3,24 +3,86 @@ package dev.moriamap.model;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import java.util.NoSuchElementException;
+import java.util.ArrayList;
+import java.util.List;
 
 class GraphTest {
     class DummyGraph extends Graph {}
     class DummyVertex extends Vertex {}
+    class DummyEdge extends Edge {
+        public DummyEdge() { super(new DummyVertex(), new DummyVertex()); }
+        public DummyEdge(Vertex from, Vertex to) { super(from, to); }
+        public double getWeight() { return 0; }
+    }
 
-    @Test void getOutgoingEdgesOfNullVertexThrowsException() {
-        Graph g = new DummyGraph();
+    @Test void verticesOfNewGraphIsEmptyList() {
+        Graph sut = new DummyGraph();
+        assertTrue(sut.getVertices().isEmpty());
+    }
+
+    @Test void edgesOfNewGraphIsEmptyList() {
+        Graph sut = new DummyGraph();
+        assertTrue(sut.getEdges().isEmpty());
+    }
+
+    @Test void addNullVertexThrowsException() {
+        Graph sut = new DummyGraph();
         assertThrows(
             IllegalArgumentException.class,
-            () -> g.getOutgoingEdgesOf(null)
+            () -> sut.addVertex(null)
         );
     }
 
-    @Test void getOutgoingEdgesOfNotContainedVertexThrowsException() {
-        Graph g = new DummyGraph();
+    @Test void verticesOfGraphWithOneVertexReturnsVertex() {
+        Graph sut = new DummyGraph();
+        Vertex vertex = new DummyVertex();
+        sut.addVertex(vertex);
+        assertTrue(sut.getVertices().contains(vertex));
+    }
+
+    @Test void addNullEdgeThrowsException() {
+        Graph sut = new DummyGraph();
+        assertThrows(IllegalArgumentException.class, () -> sut.addEdge(null));
+    }
+
+    @Test void addEdgeWithTwoNewVerticesAddsTwoNewVertices() {
+        Graph sut = new DummyGraph();
+        Edge edge = new DummyEdge();
+        sut.addEdge(edge);
+        assertEquals(2, sut.getVertices().size());
+    }
+
+    @Test void getOutgoindEdgesOfNullVertexThrowsIllegalArgumentException() {
+        Graph sut = new DummyGraph();
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> sut.getOutgoingEdgesOf(null)
+        );
+    }
+
+    @Test void getOutgoingEdgesOfAbsentVertexThrowsIllegalArgumentException() {
+        Graph sut = new DummyGraph();
+        Vertex dummy = new DummyVertex();
         assertThrows(
             NoSuchElementException.class,
-            () -> g.getOutgoingEdgesOf(new DummyVertex())
+            () -> sut.getOutgoingEdgesOf(dummy)
         );
+    }
+
+    @Test void getOutgoingEdgesOfVertexWithoutOutgoingEdgesIsEmptyList() {
+        Graph sut = new DummyGraph();
+        Vertex from = new DummyVertex();
+        sut.addVertex(from);
+        assertTrue(sut.getOutgoingEdgesOf(from).isEmpty());
+    }
+
+    @Test void getOutgoingEdgesOfVertexWithOneOutgoingEdgesReturnsSuchList() {
+        Graph sut = new DummyGraph();
+        Vertex from = new DummyVertex();
+        Edge edge = new DummyEdge(from, new DummyVertex());
+        sut.addEdge(edge);
+        List<Edge> outgoingEdges = new ArrayList<>();
+        outgoingEdges.add(edge);
+        assertEquals(outgoingEdges, sut.getOutgoingEdgesOf(from));
     }
 }
