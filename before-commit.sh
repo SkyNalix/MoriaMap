17e758b37241d7b8be9fdc4457c3a8b81ae21a28
Adrian H <adrian.heouairi@etu.u-paris.fr>
2023-04-01 22:13:28 +0200

    First version of TransportNetwork

--- before-commit.sh
+++ before-commit.sh
@@ -5,7 +5,7 @@ repo_root=$(git rev-parse --show-toplevel) || exit
 cd -- "$repo_root" || exit
 
 if ./gradlew javadoc build test; then
-  echo 'No errors found, you can commit'
+  echo 'No errors found, you can commit after checking SonarQube'
 else
   echo 'Errors encountered, fix them before committing'
 fi
