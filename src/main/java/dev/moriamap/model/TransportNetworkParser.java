55941808f90949e9b4c917405a419a6c5d7cd8c0
lineres <73391287+lineres@users.noreply.github.com>
2023-04-02 12:45:56 +0200

    fix for rebase

--- src/main/java/dev/moriamap/model/TransportNetworkParser.java
+++ src/main/java/dev/moriamap/model/TransportNetworkParser.java
@@ -7,57 +7,96 @@ import java.util.Scanner;
 
 public class TransportNetworkParser {
 
-    //TODO: 27/03/2023 add the Stop to their Line
-    /**
-     * @param fileName the name of the file with the extension
-     * @return a List<Line> containing all the Line and its variant from the file given
-     */
-    public static List<Line> load(String file_path_and_name){ 
-        try {
-            ArrayList<Line> result = new ArrayList<>();
-            ArrayList<String> listOfIdLine = new ArrayList<>();
-            File file = new File(file_path_and_name);
+    public void algorithm(List<String> tuples){
+        List<Stop> stops = new ArrayList<>();
+        List<Variant> variants = new ArrayList<>();
+        List<Line> lines = new ArrayList<>();
 
-            Scanner sc = new Scanner(file);
-            while(sc.hasNextLine()){
-                String str = sc.nextLine();
-                String[] array = str.split(";");
-                
-                String[] tmp = array[4].split(" ");
-                
-                Line line = Line.of(tmp[0]);
-                if(!listOfIdLine.contains(tmp[0])){ // add the line
-                    result.add(line);
-                    listOfIdLine.add(tmp[0]);
-                }
+        for(int i =0; i < tuples.size();i++){
+            Stop s1 = null;
+            Stop s2 = null;
+            
+            String t = tuples.get(i);
 
-                //add Variant
-                boolean x = true;
-                for(int i =0; i < result.size();i++){
-                    Variant variant = Variant.empty(Integer.parseInt(tmp[2]), line);
-                    if(!result.get(i).containsVariant(variant)  && tmp[0].equals(result.get(i).getName()) ){
-
-                            for(int j=0; j < result.get(i).getVariants().size();j++){ //if not this they are two variant 1
-                                if(result.get(i).getVariants().get(j).getId() == Integer.parseInt(tmp[2]))
-                                    x = false;
-                            }
-                        
-                        if (x)
-                            result.get(i).addVariant(variant);
-                    }
-                }
-
-                //create Stop
-                String[] gp = array[1].split(",");
-                Stop stop = Stop.from(array[0],GeographicPosition.at(Double.parseDouble(gp[0]), Double.parseDouble(gp[1])));
+            List<Stop> tupple_test = stringToStop(t);
+            if(stops.contains( tupple_test.get(0) )){ // changer le contains / equals
+                s1 = tupple_test.get(0);
+            }else{
+                s1 = tupple_test.get(0);
+                stops.add(s1);
             }
-            sc.close();
-            return result;
 
-        }catch(Exception e){
-            throw new IllegalArgumentException("File name not found in TransportNetworkParser");
+            if(stops.contains( tupple_test.get(1) )){
+                s2 = tupple_test.get(1);
+            }else{
+                s2 = tupple_test.get(1);
+                stops.add(s1);
+            }
+
+            Line l = null;
+            if(lines.contains(stringToLine(t)) ){
+                l = stringToLine(t);
+            }else{
+                l = stringToLine(t);
+                lines.add(l);
+            }
+
+            Variant v = null;
+            if(variants.contains( stringToVariant(t))){
+                v = stringToVariant(t);
+            }else{
+                v = stringToVariant(t);
+                variants.add(v);
+                l.addVariant(v);
+            }
+
+            String distance = t.split(";")[6];
+            String time = t.split(";")[5];
+            // TransportSegemnt segment = new TransportSegment(S1,S2,time,distance,v);
+            // ajouter SEGMENT à la liste des arêtes sortantes de S1
+            //ajouter SEGMENT au variant V
+            // return new TransportNetwork(stops,lines);
         }
     }
+
+    public static List<Stop> stringToStop(String str){
+        String[] array = str.split(";"); // s1
+        String[] lattitude_longitudeS1 = array[1].split(",");
+        String[] lattitude_longitudeS2 = array[2].split(",");
+
+        // GeographicPosition gp1 =  GeographicPosition.from(lattitude_longitudeS1[0],lattitude_longitudeS1[1]);
+        // GeographicPosition gp2 =  GeographicPosition.from(lattitude_longitudeS2[0],lattitude_longitudeS2[1]);
+        // Stop stop1 = Stop.from(array[0],gp1);
+        // Stop stop2 = Stop.from(array[2],gp2)
+        // return List.of(gp1,gp2);
+
+        return null;
+    }
+
+    public static Line stringToLine(String str){
+        String line_variant = str.split(";")[4];
+        return Line.of((line_variant.split(" "))[0]);
+    }
+
+    public static Variant stringToVariant(String str){
+        String line_variant = str.split(";")[4];
+
+        Line line = Line.of((line_variant.split(" "))[0]);
+        int id = Integer.parseInt((line_variant.split(" "))[2]);
+
+        return Variant.empty(id ,line);
+    }
+
+
+    /*public static void main(String[] args){
+        List<Line> l = new ArrayList<>();
+        Line a = Line.of("14");
+        Line b = Line.of("14");
+
+        l.add(a);
+        if(l.contains(b)){System.out.println("b");}
+
+    } */
 
 
 }
