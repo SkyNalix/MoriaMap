d1f06d8987d4f86051280ca86b2e588a0dbc2c73
Tazouev Arbi <tazouev.arbi@gmail.com>
2023-04-06 22:52:50 +0200

    completed main for basic dsf use

--- src/main/java/dev/moriamap/Main.java
+++ src/main/java/dev/moriamap/Main.java
@@ -1,7 +1,35 @@
 package dev.moriamap;
+import dev.moriamap.model.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 class Main {
+
 	public static void main(String[] args) {
-        throw new UnsupportedOperationException("Can not run");
+		System.out.println( "-- MoriaMap --" );
+
+		TransportNetwork tn = null;
+		if(tn == null) {
+			System.out.println( "An issue occured when creating the transport network" );
+			System.exit( 1 );
+		}
+		Scanner inputScanner = new Scanner(System.in);
+		while(true) {
+			System.out.print( "Name of the starting stop: " );
+			String start_str = inputScanner.nextLine();
+			if(start_str.isBlank()) break;
+			System.out.print( "Name of the target stop: " );
+			String end_str = inputScanner.nextLine();
+			if(end_str.isBlank()) break;
+
+			Stop start = tn.getStopByName( start_str );
+			Stop end = tn.getStopByName( end_str );
+			Map<Vertex, Edge> dfs = tn.depthFirstSearch( start );
+			List<Edge> path = Graph.getRouteFromTraversal(dfs, start, end);
+			PrettyPrinter.printEdgePath( path );
+		}
     }
+
 }
---
+++ src/main/java/dev/moriamap/model/PrettyPrinter.java
@@ -0,0 +1,24 @@
+package dev.moriamap.model;
+
+import java.util.List;
+
+
+/**
+ * Class for printing different elements of the project
+ */
+public class PrettyPrinter {
+
+	/**
+	 * Method that print the path
+	 * @param path list of edges to print
+	 */
+	public static void printEdgePath( List<Edge> path ) {
+		if(path.size() == 0)
+			return;
+		System.out.print( path.get(0).getFrom() );
+		for(Edge edge : path) {
+			System.out.print( " --> " + edge.getTo() );
+		}
+	}
+
+}
