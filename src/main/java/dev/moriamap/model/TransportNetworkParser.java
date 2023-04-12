2e321aa4c8a3e6a6a042fee1f8a404adb362de88
DERVISHI <sevidervishi@outlook.fr>
2023-04-12 10:30:23 +0200

    Renamed EdgeTuple.

--- src/main/java/dev/moriamap/model/TransportNetworkParser.java
+++ src/main/java/dev/moriamap/model/TransportNetworkParser.java
@@ -15,10 +15,10 @@ public class TransportNetworkParser {
      * @param tuples a List of EdgeTuple
      * @return a TransportNetwork corrsponding to the network given in argument
      */
-    public static TransportNetwork generateFromEdgeTuple(List<EdgeTuple> tuples ){
+    public static TransportNetwork generateFromEdgeTuple(List<EdgeRecord> tuples ){
         TransportNetwork tn = TransportNetwork.empty();
 
-        for(EdgeTuple t : tuples){
+        for(EdgeRecord t : tuples){
             
             Stop s1 = generateStop(tn,t.fromName(), t.fromLatitude(), t.fromLongitude());
             Stop s2 = generateStop(tn,t.toName(), t.toLatitude(), t.toLongitude());
@@ -44,7 +44,7 @@ public class TransportNetworkParser {
      * @return a TransportNetwork corrsponding to the network given in argument
      */
     public static TransportNetwork generateFrom(InputStream transportNetworkFileContent) throws InconsistentCSVException{
-        List<EdgeTuple> tuples = EdgeTuple.fromTuples(CSVParser.extractLines(transportNetworkFileContent));
+        List<EdgeRecord> tuples = EdgeRecord.fromTuples(CSVParser.extractLines(transportNetworkFileContent));
         return generateFromEdgeTuple(tuples);
     }
     /**
@@ -52,7 +52,7 @@ public class TransportNetworkParser {
      * @param t the actual EdgeTuple that we are browsing
      * @return the Variant needed for the transportSegment contained in the Line or a new Variant
      */
-    private static Variant generateVariant(Line l,EdgeTuple t){
+    private static Variant generateVariant(Line l,EdgeRecord t){
         Variant v = null;
         for(Variant lv : l.getVariants()){
             if (lv.getName().equals(t.variantName()) && lv.getLineName().equals(t.lineName()))
@@ -72,7 +72,7 @@ public class TransportNetworkParser {
      * @param tn the TransportNetwork
      * @return the Line contained in the TransportNetwork or if null a new Line 
      */
-    private static Line generateLine(EdgeTuple t,TransportNetwork tn){
+    private static Line generateLine(EdgeRecord t,TransportNetwork tn){
         Line l = tn.findLine(t.lineName());
         if(l == null){
             l = Line.of(t.lineName());
