d02576be952010d6390d1d2ef898a040027eb3d2
Tazouev Arbi <tazouev.arbi@gmail.com>
2023-04-15 21:10:39 +0200

    Changed Plan0Query into two methods

---
+++ src/main/java/dev/moriamap/model/QueryFailureException.java
@@ -0,0 +1,9 @@
+package dev.moriamap.model;
+
+
+/**
+ * This exception is thrown during a query's execution to signal an issue in the inputs
+ */
+public class QueryFailureException extends Exception{
+
+}
