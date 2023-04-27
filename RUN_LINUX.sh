95f3948889aaa5f463f57769f5731320a3b3d3e5
TAZOUEV Arbi <tazouev.arbi@gmail.com>
2023-04-27 19:05:38 +0200

    fixed path to jar

--- RUN_LINUX.sh
+++ RUN_LINUX.sh
@@ -3,11 +3,11 @@
 dir=$(dirname -- "$0")
 
 if [ -t 0 ]; then # If we are in an interactive terminal
-    java -jar "$dir"/build/libs/MoriaMap.jar
+    java -jar "$dir"/MoriaMap.jar
 else
     if which gnome-terminal; then
-        gnome-terminal -- $SHELL -ic 'java -jar "$0"/build/libs/MoriaMap.jar; $SHELL' "$dir"
+        gnome-terminal -- $SHELL -ic 'java -jar "$0"/MoriaMap.jar; $SHELL' "$dir"
     elif which konsole; then
-        konsole -e $SHELL -ic 'java -jar "$0"/build/libs/MoriaMap.jar; $SHELL' "$dir"
+        konsole -e $SHELL -ic 'java -jar "$0"/MoriaMap.jar; $SHELL' "$dir"
     fi
 fi
