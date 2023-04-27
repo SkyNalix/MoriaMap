c7d7d91c143f8848fb7507f76cfc87791769cf7f
Tazouev Arbi <tazouev.arbi@gmail.com>
2023-04-27 14:22:02 +0200

    Added failsafe when dirpath contain spaces or other special characters

--- RUN_LINUX.sh
+++ RUN_LINUX.sh
@@ -3,11 +3,11 @@
 dir=$(dirname -- "$0")
 
 if [ -t 0 ]; then # If we are in an interactive terminal
-    java -jar "$dir"/MoriaMap.jar
+    java -jar "$dir"/build/libs/MoriaMap.jar
 else
     if which gnome-terminal; then
-        gnome-terminal -- $SHELL -ic "java -jar '$dir'/MoriaMap.jar; $SHELL"
+        gnome-terminal -- $SHELL -ic 'java -jar "$0"/build/libs/MoriaMap.jar; $SHELL' "$dir"
     elif which konsole; then
-        konsole -e $SHELL -ic "java -jar '$dir'/MoriaMap.jar; $SHELL"
+        konsole -e $SHELL -ic 'java -jar "$0"/build/libs/MoriaMap.jar; $SHELL' "$dir"
     fi
 fi
