fbfcd1c86a3126d9a99b7eda5ac76a3065264444
Tazouev Arbi <tazouev.arbi@gmail.com>
2023-04-30 19:04:23 +0200

    Added a message when failed to create a query

--- src/main/java/dev/moriamap/Main.java
+++ src/main/java/dev/moriamap/Main.java
@@ -242,6 +242,8 @@ class Main {
             if (query != null) {
                 query.execute(tn);
                 print("\n");
+            } else {
+                print("Failed to create a query, check your inputs and repeat");
             }
         }
     }
