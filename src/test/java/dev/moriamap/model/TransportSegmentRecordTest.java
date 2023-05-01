c42889c789c263f4f08b109984ba860767a1a696
Pierre SAMSON <pierre.samson@netcourrier.com>
2023-05-01 14:16:32 +0200

    fix typos

--- src/test/java/dev/moriamap/model/TransportSegmentRecordTest.java
+++ src/test/java/dev/moriamap/model/TransportSegmentRecordTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestInstance( TestInstance.Lifecycle.PER_CLASS )
-class TransportRecordTest {
+class TransportSegmentRecordTest {
 
     TransportSegmentRecord e = new TransportSegmentRecord(
             "Olympiades",
